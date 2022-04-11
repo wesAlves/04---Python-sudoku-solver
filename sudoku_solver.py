90f80fb31cc8b90f4b624b12cefec78f16dc2383
Wes <wal2.designer@gmail.com>
2022-04-11 20:01:04 +0100

    wip: nothing acheived

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -5,8 +5,6 @@
 # Each column must have all number from 1 to 9
 # Each 3x3 must have all numbers from 1 to 9
 
-from tokenize import group
-
 
 my_sudoku = {
     'group1': [
@@ -58,52 +56,105 @@ my_sudoku = {
 
 my_sudoku_lines = {
     'line1': [
-        my_sudoku['group1'][0],
-        my_sudoku['group2'][0],
-        my_sudoku['group3'][0],
+        my_sudoku['group1'][0][0],
+        my_sudoku['group1'][0][1],
+        my_sudoku['group1'][0][2],
+        my_sudoku['group2'][0][0],
+        my_sudoku['group2'][0][1],
+        my_sudoku['group2'][0][2],
+        my_sudoku['group3'][0][0],
+        my_sudoku['group3'][0][1],
+        my_sudoku['group3'][0][2],
     ],
     'line2': [
-        my_sudoku['group1'][1],
-        my_sudoku['group2'][1],
-        my_sudoku['group3'][1],
+        my_sudoku['group1'][1][0],
+        my_sudoku['group1'][1][1],
+        my_sudoku['group1'][1][2],
+        my_sudoku['group2'][1][0],
+        my_sudoku['group2'][1][1],
+        my_sudoku['group2'][1][2],
+        my_sudoku['group3'][1][0],
+        my_sudoku['group3'][1][1],
+        my_sudoku['group3'][1][2],
     ],
     'line3': [
-        my_sudoku['group1'][2],
-        my_sudoku['group2'][2],
-        my_sudoku['group3'][2],
+        my_sudoku['group1'][2][0],
+        my_sudoku['group1'][2][1],
+        my_sudoku['group1'][2][2],
+        my_sudoku['group2'][2][0],
+        my_sudoku['group2'][2][1],
+        my_sudoku['group2'][2][2],
+        my_sudoku['group3'][2][0],
+        my_sudoku['group3'][2][1],
+        my_sudoku['group3'][2][2],
     ],
     'line4': [
-        my_sudoku['group4'][0],
-        my_sudoku['group5'][0],
-        my_sudoku['group6'][0],
+        my_sudoku['group4'][0][0],
+        my_sudoku['group4'][0][1],
+        my_sudoku['group4'][0][2],
+        my_sudoku['group5'][0][0],
+        my_sudoku['group5'][0][1],
+        my_sudoku['group5'][0][2],
+        my_sudoku['group6'][0][0],
+        my_sudoku['group6'][0][1],
+        my_sudoku['group6'][0][2],
     ],
     'line5': [
-        my_sudoku['group4'][1],
-        my_sudoku['group5'][1],
-        my_sudoku['group6'][1],
+        my_sudoku['group4'][1][0],
+        my_sudoku['group4'][1][1],
+        my_sudoku['group4'][1][2],
+        my_sudoku['group5'][1][0],
+        my_sudoku['group5'][1][1],
+        my_sudoku['group5'][1][2],
+        my_sudoku['group6'][1][0],
+        my_sudoku['group6'][1][1],
+        my_sudoku['group6'][1][2],
     ],
     'line6': [
-        my_sudoku['group4'][2],
-        my_sudoku['group5'][2],
-        my_sudoku['group6'][2],
+        my_sudoku['group4'][2][0],
+        my_sudoku['group4'][2][1],
+        my_sudoku['group4'][2][2],
+        my_sudoku['group5'][2][0],
+        my_sudoku['group5'][2][1],
+        my_sudoku['group5'][2][2],
+        my_sudoku['group6'][2][0],
+        my_sudoku['group6'][2][1],
+        my_sudoku['group6'][2][2],
     ],
     'line7': [
-        my_sudoku['group7'][0],
-        my_sudoku['group8'][0],
-        my_sudoku['group9'][0],
+        my_sudoku['group7'][0][0],
+        my_sudoku['group7'][0][1],
+        my_sudoku['group7'][0][2],
+        my_sudoku['group8'][0][0],
+        my_sudoku['group8'][0][1],
+        my_sudoku['group8'][0][2],
+        my_sudoku['group9'][0][0],
+        my_sudoku['group9'][0][1],
+        my_sudoku['group9'][0][2],
     ],
     'line8': [
-        my_sudoku['group7'][1],
-        my_sudoku['group8'][1],
-        my_sudoku['group9'][1],
+        my_sudoku['group7'][1][0],
+        my_sudoku['group7'][1][1],
+        my_sudoku['group7'][1][2],
+        my_sudoku['group8'][1][0],
+        my_sudoku['group8'][1][1],
+        my_sudoku['group8'][1][2],
+        my_sudoku['group9'][1][0],
+        my_sudoku['group9'][1][1],
+        my_sudoku['group9'][1][2],
     ],
     'line9': [
-        my_sudoku['group7'][2],
-        my_sudoku['group8'][2],
-        my_sudoku['group9'][2],
+        my_sudoku['group7'][2][0],
+        my_sudoku['group7'][2][1],
+        my_sudoku['group7'][2][2],
+        my_sudoku['group8'][2][0],
+        my_sudoku['group8'][2][1],
+        my_sudoku['group8'][2][2],
+        my_sudoku['group9'][2][0],
+        my_sudoku['group9'][2][1],
+        my_sudoku['group9'][2][2],
     ],
 }
-
 
 my_sudoku_columns = {
     "column1": [
@@ -209,5 +260,46 @@ my_sudoku_columns = {
 
 }
 
-print(my_sudoku_lines['line1'])
-print(my_sudoku_columns['column1'])
+# print(len(my_sudoku_lines['line1']))
+# print(len(my_sudoku_columns['column1']))
+
+
+# for i in my_sudoku_lines:
+#     print(i)
+
+# for i in my_sudoku_columns:
+#     print(i)
+group_number = 0
+line_number = 0
+
+for i in my_sudoku:
+    None
+
+lines = my_sudoku_lines
+columns = my_sudoku_columns
+
+banana = ''
+for line in lines:
+
+    for i in range(len(lines[line])):
+
+        for values in lines[line]:
+
+            if lines[line][i] != values:
+
+                banana = 'not equal'
+
+print(banana)
+
+# if my_sudoku_lines[line][group_number] == "Col1 row 1":
+
+#     line_value = my_sudoku_lines[line][0]
+#     print(my_sudoku_lines[line][0])
+
+# for column in columns:
+
+#     if columns[column][line_number] == line_value:
+#         line_number = line_number + 1
+#         print("is equal")
+
+group_number = group_number+1

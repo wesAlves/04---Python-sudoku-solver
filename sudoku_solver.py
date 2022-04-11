9b52ce6345612b336e252db0c5005ea244e1c8d4
Wes <wal2.designer@gmail.com>
2022-04-11 15:55:07 +0100

    wip: new try

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,12 +1,18 @@
 # define an sudoku table as 3x3 and 5 in the middle
 
-line1 = [0] * 3
-line2 = [0] * 3
-line3 = [0] * 3
+row1 = {'col1': 0, 'col2': 0, 'col3': 0}
+row2 = {'col1': 0, 'col2': 0, 'col3': 0}
+row3 = {'col1': 0, 'col2': 0, 'col3': 0}
 
-line2[1] = 5
+# row2['col2'] = 5
 
-my_sudoku_arr = line1 + line2 + line3
+my_sudoku_arr = {
+    "row1": row1,
+    "row2": row2,
+    "row3": row3,
+}
+
+# print(my_sudoku_arr)
 
 
 def write_numbers(*args):
@@ -22,7 +28,7 @@ def write_numbers(*args):
     print(seted_numbers)
 
 
-write_numbers(1, 2, 3)
+# write_numbers(1, 2, 3)
 
 
 # fisrt group 3x3 is a list, and it has 3 positions at line1(col1, col2, col3), line2(col1, col2, col3) and line3(col1, col2, col3)

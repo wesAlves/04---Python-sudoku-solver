d05fb1c3232409c314e05b8ab3bbda4b7da6752a
Wes <wal2.designer@gmail.com>
2022-05-04 17:31:00 +0100

    in the way

--- solve_01.py
+++ solve_01.py
@@ -22,33 +22,27 @@ usedIn_in_row = []
 def Is_in_main_numbers(my_number):
     if my_number in main_numbers:
         usedIn_in_row.append(my_number)
-        # print(usedIn_in_row)
+
         return my_number
     else:
         if my_number in usedIn_in_row:
-            print("already in used try another")
             n = 0
-            while my_number not in usedIn_in_row:
-                my_number = main_numbers[n]
 
-                if my_number not in usedIn_in_row:
-                    usedIn_in_row.append(my_number)
-                    print(my_number)
-                    print("ends here")
+            while n in range(len(main_numbers)):
 
-                n = n+1
+                if main_numbers[n] in usedIn_in_row:
+                    n = n+1
+                else:
+                    usedIn_in_row.append(main_numbers[n])
+                    break
 
-        # else:
-        #     print("Is not in the list adding now")
-        #     usedIn_in_row.append(my_number)
-        # return my_number
-        # for row in my_sudoku_arr:
-        #     for row_item in row:
-
-        # print('No it is not')
+        else:
+            usedIn_in_row.append(my_number)
+            return 3
 
 
 for row in my_sudoku_arr:
     for row_item in row:
         row_item = Is_in_main_numbers(row_item)
         # print(row_item)
+print(usedIn_in_row)

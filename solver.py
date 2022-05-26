591ee83277926b1d3913937d504511327aed64f9
Wes <wal2.designer@gmail.com>
2022-05-26 11:53:16 +0100

    creating lines and columns

--- solver.py
+++ solver.py
@@ -1,23 +1,29 @@
 def sudoku_solver(sudokuOBJ):
 
+    sudoku_row_size = 9
+    sudoku_col_size = 9
+    group_size = 3
+
     # Create arrays for line, column and 3X3 square
     lines = {}
 
-    columns = []
+    columns = {}
 
     i_line = 0
     i_column = 0
 
     for line in sudokuOBJ:
-        lines['line_%s' % i_line] = line
+        lines[f'line_{i_line}'] = line
         i_line += 1
 
-    # for key in lines:
-    #     print(len(lines[key]))
-    #     print(key)
+    for i_column in range(sudoku_col_size):
+        current_column = columns[f'column_{i_column}'] = []
+
+        for i in range(len(lines)):
+            current_column.append(lines[f'line_{i}'][i_column])
 
     # print('linhas', lines)
-    # print('colunas', columns)
+    print('colunas', columns)
 
 
 sudoku_solver([[8, 7, 4, 6, 9, 5, 0, 2, 0],

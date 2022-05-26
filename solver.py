bf61fdfd006e77a9e8b7a297b8bd6e83fc0f9615
Wes <wal2.designer@gmail.com>
2022-05-26 19:16:12 +0100

    wip

--- solver.py
+++ solver.py
@@ -8,8 +8,11 @@ def sudoku_solver(sudokuOBJ):
 
     lines = []
     columns = []
-    group = [[0] * 3] * 3
-    groups = [group] * 9
+    # group = []
+    groups = [[[0] * 3] * 3] * 9
+
+    x = 0
+    y = 0
 
     # populate line
     for line in sudoku_dic:
@@ -17,14 +20,44 @@ def sudoku_solver(sudokuOBJ):
 
     # populate columns
     for i in range(sudoku_size):
-
         columns.append([])
 
         for line in lines:
             columns[i].append(line[i])
 
-    print(columns)
-    print(lines)
+    # populate group
+
+    for group in groups:
+        print(x)
+        # print(lines[x])
+
+        for line in group:
+            # print(line)
+            line[0] = (x, y)
+            line[1] = (x+1, y+1)
+            line[2] = (x+2, y+2)
+
+            print('group line')
+
+            y += 1
+            # for i in range(len(line)):
+            #     # print(i)
+            #     print(line)
+        #         if y < 9:
+        #             print(y)
+        #             line[i] = lines[x][y]
+        #             y += 1
+        #         else:
+        #             y = 0
+        #             line[i] = lines[x][y]
+        #             y += 1
+
+        print(group)
+        x += 1
+
+    # print(columns)
+    # print(lines)
+    # print(groups)
 
     # go by line
     # for line in sudoku_dic:
@@ -55,11 +88,12 @@ def sudoku_solver(sudokuOBJ):
 
 sudoku_solver([
     [8, 7, 4, 6, 9, 5, 0, 2, 0],
-    #    [0, 3, 0, 0, 7, 4, 0, 0, 6],
-    #    [6, 9, 5, 3, 1, 2, 0, 0, 7],
-    #    [5, 2, 6, 0, 4, 8, 0, 3, 0],
-    #    [0, 0, 0, 5, 0, 7, 8, 6, 4],
-    #    [0, 0, 0, 9, 0, 0, 0, 0, 0],
-    #    [1, 0, 8, 0, 3, 6, 0, 0, 2],
-    #    [0, 0, 2, 0, 5, 9, 1, 7, 8],
-    [7, 5, 9, 2, 8, 1, 0, 4, 3]])
+    [0, 3, 0, 0, 7, 4, 0, 0, 6],
+    [6, 9, 5, 3, 1, 2, 0, 0, 7],
+    [5, 2, 6, 0, 4, 8, 0, 3, 0],
+    [0, 0, 0, 5, 0, 7, 8, 6, 4],
+    [0, 0, 0, 9, 0, 0, 0, 0, 0],
+    [1, 0, 8, 0, 3, 6, 0, 0, 2],
+    [0, 0, 2, 0, 5, 9, 1, 7, 8],
+    [7, 5, 9, 2, 8, 1, 0, 4, 3]
+])
